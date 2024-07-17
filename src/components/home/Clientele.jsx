66e0389a71fcadee14f0b5d44d13f769d2f3d8a0
upgraded57 <shokunbiupgraded@gmail.com>
2024-07-17 14:57:25 +0100

    Added clientelle carousel animation

--- src/components/home/Clientele.jsx
+++ src/components/home/Clientele.jsx
@@ -27,11 +27,20 @@ export default function Clientele() {
     clientele_img_11,
   ];
   return (
-    <section className="px-[4vw] mb-10  w-full max-w-[1440px] mx-auto overflow-x-hidden">
+    <section className="px-[4vw] mb-10  w-full max-w-screen-xl mx-auto overflow-x-hidden">
       <h1 className="h-text text-grey-clr text-center uppercase">
         Our Clientele
       </h1>
-      <div className="carousel w-full gap-10 mt-10">
+      <div className="carousel-container carousel w-full gap-10 mt-10">
+        {clientImgs.map((image, idx) => (
+          <div key={idx} className="carousel-item h-10 relative">
+            <img
+              src={image}
+              alt="Clientele"
+              className="grayscale opacity-50 hover:grayscale-0 hover:opacity-100"
+            />
+          </div>
+        ))}
         {clientImgs.map((image, idx) => (
           <div key={idx} className="carousel-item h-10 relative">
             <img
