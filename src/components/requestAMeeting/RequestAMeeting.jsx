b54b35ef76f59dd6ebe8c9ce27204546ffaa8cec
upgraded57 <shokunbiupgraded@gmail.com>
2024-08-10 12:50:00 +0100

    Added calendly integration

--- src/components/requestAMeeting/RequestAMeeting.jsx
+++ src/components/requestAMeeting/RequestAMeeting.jsx
@@ -1,23 +1,28 @@
 import "../home/home.css";
-import RequestForm from "./RequestForm";
 
 export default function RequestAMeeting() {
+  const openCalendly = () => {
+    Calendly.initPopupWidget({ url: "https://calendly.com/ijm-africa/30min" });
+    return false;
+  };
   return (
     <div className="px-[4vw]">
-      <section className="request_a_meeting py-10 my-10 bg-green-clr rounded-[20px] md:rounded-[40px] flex flex-col md:flex-row items-start md:items-center gap-10 w-full max-w-screen-lg mx-auto">
-        <div className="basis-full md:basis-1/2 px-[4vw]">
-          <h2 className="text-2xl md:text-3xl lg:text-4xl text-white pb-6">
-            Request a Meeting
-          </h2>
-          <p className="text-white text-18px leading-loose">
-            Need a personalized service or you&apos;ll like to reach out to us
-            personally? We&apos;re here to help you. Start by filling out the
-            contact form and you&apos;ll hear from us as soon as possible.
-          </p>
-        </div>
-        <div className="basis-full md:basis-1/2 px-[4vw] w-full">
-          <RequestForm />
-        </div>
+      <section className="request_a_meeting py-10 px-[4vw] text-center my-10 bg-green-clr rounded-[20px] md:rounded-[40px] w-full max-w-screen-lg mx-auto">
+        <h2 className="text-2xl md:text-3xl lg:text-4xl text-white pb-3">
+          Request a Meeting
+        </h2>
+        <p className="text-white text-18px leading-loose">
+          Need a personalized service or you'll like to reach out to us
+          personally? We're here to help you. Start by filling out the contact
+          form and you'll hear from us as soon as possible.
+        </p>
+
+        <button
+          className="btn btn-wide mt-6 bg-blue-clr hover:bg-blue-clr border-0 text-white font-[500]"
+          onClick={openCalendly}
+        >
+          Request a Meeting
+        </button>
       </section>
     </div>
   );
